d076262c8296af7629730a82c2e4d158855911cc
Zach S.L <zach.s.ledford@gmail.com>
2020-07-01 11:17:24 -0400

    adding connection to google books api and receiving response

--- google-book-search/src/components/Books.js
+++ google-book-search/src/components/Books.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import SearchArea from "../components/booksComponents/SearchArea";
+import request from "superagent"
 
 class Books extends Component {
   constructor(props) {
@@ -10,15 +11,24 @@ class Books extends Component {
     };
   }
 
+  searchBook = (event) => {
+      event.preventDefault();
+      request.get("https://www.googleapis.com/books/v1/volumes").query({
+          q: this.state.searchField
+      }).then((data)=> {
+          console.log(data)
+      })
+  };
+
   handleSearch = (event) => {
-      console.log(event.target.value);
+    console.log(event.target.value);
     this.setState({ searchField: event.target.value });
   };
 
   render() {
     return (
       <div>
-        <SearchArea handleSearch={this.handleSearch}/>
+        <SearchArea searchBook={this.searchBook}  handleSearch={this.handleSearch} />
       </div>
     );
   }
