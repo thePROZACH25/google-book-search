75758f2addc48a0d769f4a68a171f08821f8ac1c
Zach S.L <zach.s.ledford@gmail.com>
2020-07-31 08:04:54 -0400

    pushing to heroku

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import Axios from "axios";
 
 export default {
   getBook: function (query) {
-    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    return Axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   },
   // Deletes book using given id
   deleteBook: function (id) {
-    return axios.delete("/api/books" + id).then((result) => result.data);
+    return Axios.delete("/api/books" + id).then((result) => result.data);
   },
   // Saves book to db
   saveBook: function (bookData) {
-    return axios.post("/api/books", bookData).then((result) => result.data);
+    return Axios.post("/api/books", bookData).then((result) => result.data);
   },
   // Retrieve saved books from db
   savedBooks: function () {
-    return axios.get("/api/books").then((result) => result.data);
+    return Axios.get("/api/books").then((result) => result.data);
   },
 };
